14cb046ff8425a006cccb231bb83a0e0f0577b60
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-09-17 18:53:52 -0400

    Add files via upload

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,7 @@
+﻿$Hello = "Hello, PowerShell"
+Write-Host($Hello)
+function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+write-host(getIP)
+Send-MailMessage -To "mbowtu@mail.uc.edu" -From "copambow0@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
