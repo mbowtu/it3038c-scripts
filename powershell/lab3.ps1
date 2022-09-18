de74cfaee5d9dadded9656811d0f8527dba6b2ae
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-09-17 21:02:57 -0400

    Add files via upload

---
+++ powershell/lab3.ps1
@@ -0,0 +1,8 @@
+﻿
+$IP = getIP
+$u = whoami
+$a = hostname
+$H = $HOST.Version.Major
+$g = get-date -UFormat "%A %m/%d/%y"
+Write-Host("This machine's IP is $IP. User is $u.Hostname is $a.")
+write-host("PowerShell Version $H. Today's Date is $g")
