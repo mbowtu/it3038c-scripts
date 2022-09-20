b05b8a9d9d1dcfcc698b144af1cfa9f1a6815535
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-09-20 16:03:58 -0400

    Update lab3.ps1

--- powershell/lab3.ps1
+++ powershell/lab3.ps1
@@ -1,9 +1,9 @@
-﻿
+
 $IP = getIP
 $u = whoami
 $a = hostname
 $H = $HOST.Version.Major
 $g = get-date -UFormat "%A %m/%d/%y"
 
-$BODY = "This maachine's IP is $IP. User is $u. Hostname is $a.Powershell version $H. Today's Date is $g."
+$BODY = "This machine's IP is $IP. User is $u. Hostname is $a.Powershell version $H. Today's Date is $g."
 Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "copambow0@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
