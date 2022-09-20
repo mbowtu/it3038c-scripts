12f565248bf30cd8d1616ef241a28aaedae2c3dc
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-09-20 15:15:39 -0400

    Delete lab3.ps1

--- powershell/lab3.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$IP = getIP
-$u = whoami
-$a = hostname
-$H = $HOST.Version.Major
-$g = get-date -UFormat "%A %m/%d/%y"
-Write-Host("This machine's IP is $IP. User is $u.Hostname is $a.")
-write-host("PowerShell Version $H. Today's Date is $g")
