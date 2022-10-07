59d4a8b77147d005db3bcc5ab6a5efb5a4e9f5c0
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-10-07 15:54:23 -0400

    Update nameAge.py

--- python/nameAge.py
+++ python/nameAge.py
@@ -1,5 +1,26 @@
-print('what is your name?')
-myNane = input()
-print('hello, ' + myName + '. That is a good name. How old are you?')
-myAge = input()
-print(myAge + '? That\'s funny, I\'m only a few seconds old.')
+import time
+start_time = time.time()
+print("What is your name?")
+myName = input()
+print("Hello " + myName + ". That is a good name. How old are you?")
+myAge = int(input())
+if myAge < 13:
+    print("Learning young, that's good")
+elif myAge == 13:
+    print("You're a teenager now... that's cool, I guess")
+elif myAge > 13 and myAge < 30:
+    print("Still young, still learning...")
+elif myAge >= 30 and myAge < 65:
+    print("NOw you're adulting.")
+else:
+    print("... You're getting old?")
+
+programAge = int(time.time() - start_time)
+print(myAge + "? That's funny, I'm only a few seconds old.")
+print("I wish I was " + str(myAge * 2) + " years old")
+print("---------------------")
+print("%s? That's funny, I'm only a few second old." % myAge, programAge)
+print("I wish I was %s years old" % str(myAge * 2))
+time.sleep(3)
+print("I\'m tired. I go sleep now.")
+
