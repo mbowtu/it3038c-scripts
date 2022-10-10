6f92fbafebc41e144048db7b7210be69879e9970
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-10-09 20:12:09 -0400

    Update server.js

--- node/server.js
+++ node/server.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const os = require("os");
 const ip = require('ip');
 
+
+
 http.createServer((req, res) => {
   if (req.url === "/") {
       fs.readFile("./public/index.html", "UTF-8", (err, body) => {
@@ -11,6 +13,27 @@ http.createServer((req, res) => {
     });
   } else if(req.url.match("/sysinfo")) {
     myHostName=os.hostname();
+    TotalMem = os.totalmem();
+    FreeMem = os.freemem();
+    numCpus = os.cpus().length
+
+    // Server Uptime
+    var utSec = os.uptime();
+    var utMin = utSec/60;
+    var utHour = utMin/60;
+    var utDay = utHour/24;
+
+    utSec = Math.floor(utSec);
+    utMin = Math.floor(utMin);
+    utHour = Math.floor(utHour);
+    utDay = Math.floor(utDay);
+    
+
+    utDay = utDay%24;
+    utHour = utHour%60;
+    utMin = utMin%60;
+    utSec = utSec%60;
+
     html=`
     <!DOCTYPE html>
     <html>
@@ -20,10 +43,10 @@ http.createServer((req, res) => {
       <body>
         <p>Hostname: ${myHostName}</p>
         <p>IP: ${ip.address()}</p>
-        <p>Server Uptime: </p>
-        <p>Total Memory: </p>
-        <p>Free Memory: </p>
-        <p>Number of CPUs: </p>
+        <p>Server Uptime:Days: ${utDay}, Hours: ${utHour}, Minutes: ${utMin}, Seconds: ${utSec}</p>
+        <p>Total Memory:${(TotalMem / (1024 * 1024))} MB</p>
+        <p>Free Memory:${(FreeMem / (1024 * 1024))} MB</p>
+        <p>Number of CPUs: ${numCpus}</p>
       </body>
     </html>`
     res.writeHead(200, {"Content-Type": "text/html"});
