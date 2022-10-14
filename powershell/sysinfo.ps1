d06bb9b548804f01cb414800d3619c18f89c51ec
mbowtu <112358394+mbowtu@users.noreply.github.com>
2022-10-14 10:47:21 -0400

    Update sysinfo.ps1

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -4,4 +4,7 @@ function getIP{
     (get-netipaddress).ipv4address | Select-String "192*"
 }
 write-host(getIP)
+$IP = getIP
+write-Host("This machine's IP is $IP")
+write-Host("This machine's IP is {0}" -f $IP)
 Send-MailMessage -To "mbowtu@mail.uc.edu" -From "copambow0@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
